910d2704a01af8da68481a0cae8012d655856574
K. Neelima <neeluneelima553@gmail.com>
2026-08-23 21:51:02 +0530

    Real Time Problems

---
+++ ElectricityBills.java
@@ -0,0 +1,35 @@
+package practice;
+import java.util.Scanner;
+public class ElectricityBills {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the Units Consumed: ");
+		int units = sc.nextInt();
+
+		double bill = 0;
+		
+		if (units < 0) {
+		  System.out.println("Invalid units");
+	   }
+		else if(units <= 100) {
+			bill = units * 2;
+			System.out.println("Electricity Bill: ₹" + bill);
+		}
+		else if (units <= 200) {
+			bill = (100 * 2) + ((units-100) * 3);
+			System.out.println("Electricity Bill: ₹" + bill);
+		}
+		else if (units <= 300) {
+			bill =(100 * 2) + (100 * 3) + ((units - 200) *5);
+			System.out.println("Electricity Bill: ₹" + bill);
+		}
+		else {
+			bill =(100 * 2) + (100 * 3) + (100 * 5) + ((units -300) *7);
+			System.out.println("Electricity Bill: ₹" + bill);
+		}
+	     sc.close();
+	}
+
+}
