da5c1cf921cc8aa88f253c719cbefab6394509cf
K. Neelima <neeluneelima553@gmail.com>
2026-09-02 20:57:15 +0530

    pattern programs

---
+++ Alphabet.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class Alphabet {
+
+	public static void main(String[] args) {
+		int rows =5;
+		char c='A';
+		for (int i=0;i<rows;i++) {
+			for (int j=0;j<=i;j++) 	
+		{
+			System.out.print(c);
+			c++;
+		}
+		System.out.println();
+	}
+	}
+}
