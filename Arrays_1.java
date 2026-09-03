d198134688c9cf84b825524eba75bf1d616aaf8d
K. Neelima <neeluneelima553@gmail.com>
2026-09-03 21:56:59 +0530

    Arrays Programs

---
+++ Arrays_1.java
@@ -0,0 +1,15 @@
+package practice;
+// Initializes  outside the loop and modify changes inside print Statement outside 
+public class Arrays_1 {
+
+	public static void main(String[] args) {
+		int a=0;
+		
+		for (int i=0;i<=5;i++) {
+			a=a+i;
+			System.out.println("A= "+a);
+		}
+			
+	}
+
+}
