2f844f9b47cda20dbe47612b95ee7efc9cdec275
K. Neelima <neeluneelima553@gmail.com>
2026-09-07 16:46:32 +0530

    Arrays Programs

--- Array.java
+++ Array.java
@@ -9,14 +9,13 @@ public class Array {
 ////// for loop to calculate the sum of numbers from 0 to 5. 
 ////// After adding each number, display the current value of the sum.
 		
-//		int a=0;
-//		
-//		for (int i=0;i<=5;i++) {
-//			a=a+i;
-//			System.out.println("A= "+a);
-//		}	
-//	}
-//}
+		int a=0;
+		for (int i=0;i<=5;i++) {
+			a=a+i;
+			System.out.println("A= "+a);
+		}	
+	}
+}
 
 /////// calculate the power of a given number using a for loop
 		
@@ -39,21 +38,21 @@ sc.close();
 //////for loop to print numbers from 1 to 20, where the loop variable is multiplied by 2 after 
 //////		each iteration. Display the value of i in each iteration.
 		
-//		for (int i=1; i<=20; i++) {
-//            System.out.println(i);
-//            i*= 2;
-//        }
-//    }
-//}
+		for (int i=1; i<=20; i++) {
+            System.out.println(i);
+            i*= 2;
+        }
+    }
+}
 
 
 //// why the for loop does not execute.
-//		 int i =1; 
-//		for (    ; i>10;    ) 
-//	     break;
-//		 System.out.println(i);
-//       i++;
-//	}
-//}
+		 int i =1; 
+		for (    ; i>10;    ) 
+	     break;
+		 System.out.println(i);
+       i++;
+	}
+}
 		
 	
