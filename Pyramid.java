4952653ee2eb0863a1ec718409739b3fc1e72bb4
K. Neelima <neeluneelima553@gmail.com>
2026-09-13 18:25:53 +0530

    pattern programs

---
+++ Pyramid.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+		int n=6;
+		for (int i=1;i<=n;i++) {
+			for (int j=i;j<n;j++)
+			{
+				System.out.print(" ");
+			}
+			for (int k=1;k<n;k+=2) 
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
