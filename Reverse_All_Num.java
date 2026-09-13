e2936f3b73ebb459aa30d2e3e977ccbe08e17f33
K. Neelima <neeluneelima553@gmail.com>
2026-09-13 18:26:44 +0530

    Pattern Programs

---
+++ Reverse_All_Num.java
@@ -0,0 +1,16 @@
+package Pattern;
+
+public class Reverse_All_Num {
+
+	public static void main(String[] args) {
+		int n=9;
+		for (int i=1;i<=n;i++) {
+			for (int j=1;j<=i;j++) 
+			{
+				System.out.print(n);
+			}
+			n--;
+			System.out.println();
+		}
+	}
+}
